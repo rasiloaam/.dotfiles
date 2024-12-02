b70496b55b49791f005ff4eae0319ad5dfc2c7d7
rasiloaam <rasiloaam@gmail.com>
2024-12-02 13:37:16 +0530

    dotfiles: user: neovim: Set tab and shift width

--- user/neovim/config/lua/sets.lua
+++ user/neovim/config/lua/sets.lua
@@ -1,3 +1,9 @@
 -- Line numbering
 vim.opt.nu = true
 vim.opt.relativenumber = true
+
+-- Tab and shift width
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
