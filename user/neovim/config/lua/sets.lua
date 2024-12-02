08d28003deaa4b0fc12634f5175fd25070f726cb
rasiloaam <rasiloaam@gmail.com>
2024-12-02 13:39:34 +0530

    dotfiles: user: neovim: Enable undo file and set path to undo directory

--- user/neovim/config/lua/sets.lua
+++ user/neovim/config/lua/sets.lua
@@ -7,3 +7,7 @@ vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
+
+-- Undo
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
