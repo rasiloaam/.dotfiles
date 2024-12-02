ca5ebdf808885f408cdce01f7d4d89a394daf7c0
rasiloaam <rasiloaam@gmail.com>
2024-12-02 18:27:53 +0530

    dotfiles: user: firefox: Disable crappy home activity stream page

--- user/firefox/default.nix
+++ user/firefox/default.nix
@@ -1,10 +1,30 @@
-{
+{lib, ...}: {
   # Configure Firefox
   programs.firefox = {
     enable = true;
     profiles = {
       luna = {
         isDefault = true;
+        settings = {
+          # Disable crappy home activity stream page
+          "browser.newtabpage.activity-stream.feeds.topsites" = false;
+          "browser.newtabpage.activity-stream.showSponsoredTopSites" = false;
+          "browser.newtabpage.activity-stream.improvesearch.topSiteSearchShortcuts" = false;
+          "browser.newtabpage.blocked" = lib.genAttrs [
+            # Youtube
+            "26UbzFJ7qT9/4DhodHKA1Q=="
+            # Facebook
+            "4gPpjkxgZzXPVtuEoAL9Ig=="
+            # Wikipedia
+            "eV8/WsSLxHadrTL1gAxhug=="
+            # Reddit
+            "gLv0ja2RYVgxKdp0I5qwvA=="
+            # Amazon
+            "K00ILysCaEq8+bEqV/3nuw=="
+            # Twitter
+            "T9nJot5PurhJSy8n038xGA=="
+          ] (_: 1);
+        };
       };
     };
   };
