528125ea65993bdded9716c9e4cc9caed2a12af0
rasiloaam <rasiloaam@gmail.com>
2024-12-02 19:15:46 +0530

    dotfiles: user: fish: Set custom command not found handler function

--- user/terminal/fish.nix
+++ user/terminal/fish.nix
@@ -9,5 +9,11 @@
       # Enable vi key bindings
       fish_vi_key_bindings
     '';
+    functions = {
+      __fish_command_not_found_handler = {
+        body = "echo \"$argv[1]: command not found\"";
+        onEvent = "fish_command_not_found";
+      };
+    };
   };
 }
